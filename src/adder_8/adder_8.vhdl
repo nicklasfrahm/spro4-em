c80e5444ac629939afd705cf0908cf7548779a4e
nicklasfrahm <nicklas.frahm@gmail.com>
2018-02-20 10:35:05 +0100

    Replace tab indentation with spaces

--- src/adder_8/adder_8.vhdl
+++ src/adder_8/adder_8.vhdl
@@ -2,32 +2,32 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY adder_8 IS
-	PORT (
-		a_in : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		b_in : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		carry_in : IN STD_LOGIC;
-		carry_out : OUT STD_LOGIC;
-		sum_out : OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
-	);
+  PORT (
+    a_in : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+    b_in : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+    carry_in : IN STD_LOGIC;
+    carry_out : OUT STD_LOGIC;
+    sum_out : OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+  );
 END adder_8;
 
 ARCHITECTURE Behavioral OF adder_8 IS
 BEGIN
-	PROCESS (a_in, b_in, carry_in)
+  PROCESS (a_in, b_in, carry_in)
 
-	VARIABLE vsum : std_logic_vector(7 DOWNTO 0);
-	VARIABLE carry : std_logic;
+  VARIABLE vsum : std_logic_vector(7 DOWNTO 0);
+  VARIABLE carry : std_logic;
 
-	BEGIN
-		carry := carry_in;
-		
-		FOR i IN 0 TO 7 LOOP
-			vsum(i) := (a_in(i) XOR b_in(i)) XOR carry;
-			carry := ((a_in(i) XOR b_in(i)) AND carry) OR (a_in(i) AND b_in(i));
-		END LOOP;
+  BEGIN
+    carry := carry_in;
+    
+    FOR i IN 0 TO 7 LOOP
+      vsum(i) := (a_in(i) XOR b_in(i)) XOR carry;
+      carry := ((a_in(i) XOR b_in(i)) AND carry) OR (a_in(i) AND b_in(i));
+    END LOOP;
 
-		sum_out <= vsum;
-		carry_out <= carry;
+    sum_out <= vsum;
+    carry_out <= carry;
 
-	END PROCESS;
+  END PROCESS;
 END Behavioral;
--- src/adder_8/adder_8_test.vhdl
+++ src/adder_8/adder_8_test.vhdl
@@ -12,13 +12,13 @@ END adder_8_test;
 ARCHITECTURE Behavioral OF adder_8_test IS
 
 COMPONENT adder_8
-	PORT (
-		a_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-		b_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-		carry_in : IN STD_LOGIC;
-		carry_out : OUT STD_LOGIC;
-		sum_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
-	);
+  PORT (
+    a_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    b_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    carry_in : IN STD_LOGIC;
+    carry_out : OUT STD_LOGIC;
+    sum_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+  );
 END COMPONENT;
 
 -- declare the local signals to assign values to and for observation.
@@ -40,67 +40,67 @@ SIGNAL sum_out : std_logic_vector(7 DOWNTO 0);
 --constant _period : time := 10 ns;
 
 BEGIN
-	-- create an instance of the UUT and connect it to the defined input and output signals
-	uut : adder_8 PORT MAP(
-		a_in => a_in,
-		b_in => b_in,
-		carry_in => carry_in,
-		carry_out => carry_out,
-		sum_out => sum_out
-	);
+  -- create an instance of the UUT and connect it to the defined input and output signals
+  uut : adder_8 PORT MAP(
+    a_in => a_in,
+    b_in => b_in,
+    carry_in => carry_in,
+    carry_out => carry_out,
+    sum_out => sum_out
+  );
 
-	-- clock process definitions
-	-- uncomment these process statements if you need a clock generation process and give clock the right name
-	-- this process will run in parallel with the stimulus process
-	-- _process :process
-	--begin
-	-- <= '0';
-	--wait for _period/2;
-	-- <= '1';
-	--wait for _period/2;
-	--end process;
+  -- clock process definitions
+  -- uncomment these process statements if you need a clock generation process and give clock the right name
+  -- this process will run in parallel with the stimulus process
+  -- _process :process
+  --begin
+  -- <= '0';
+  --wait for _period/2;
+  -- <= '1';
+  --wait for _period/2;
+  --end process;
 
-	-- stimulus process, this is where you define a sequence of input signals for the simulation.
-	-- all signals must be defined.
-	stimulus_process : PROCESS
-	BEGIN
-		-- hold reset state for 100 ns
-		WAIT FOR 100 ns;
+  -- stimulus process, this is where you define a sequence of input signals for the simulation.
+  -- all signals must be defined.
+  stimulus_process : PROCESS
+  BEGIN
+    -- hold reset state for 100 ns
+    WAIT FOR 100 ns;
 
-		-- uncomment the following (and give the clock the right name)
-		-- if you want the circuit to "settle" before you assign values to the inputs
-		--wait for _period*10;
+    -- uncomment the following (and give the clock the right name)
+    -- if you want the circuit to "settle" before you assign values to the inputs
+    --wait for _period*10;
 
-		-- now insert test cases here
-		a_in <= "00001111";
-		b_in <= "01010101";
-		carry_in <= '0';
-		WAIT FOR 100 ns;
-		-- expect: sum_out = 0b01100100 = 0x64
-		-- expect: carry_out = 0
+    -- now insert test cases here
+    a_in <= "00001111";
+    b_in <= "01010101";
+    carry_in <= '0';
+    WAIT FOR 100 ns;
+    -- expect: sum_out = 0b01100100 = 0x64
+    -- expect: carry_out = 0
 
-		a_in <= "00000000";
-		b_in <= "01010101";
-		carry_in <= '0';
-		WAIT FOR 100 ns;
-		-- expect: sum_out = 0b01010101 = 0x55
-		-- expect: carry_out = 0
+    a_in <= "00000000";
+    b_in <= "01010101";
+    carry_in <= '0';
+    WAIT FOR 100 ns;
+    -- expect: sum_out = 0b01010101 = 0x55
+    -- expect: carry_out = 0
 
-		a_in <= "11110000";
-		b_in <= "01010101";
-		carry_in <= '0';
-		WAIT FOR 100 ns;
-		-- expect: sum_out = 0b01000101 = 0x45
-		-- expect: carry_out = 1
+    a_in <= "11110000";
+    b_in <= "01010101";
+    carry_in <= '0';
+    WAIT FOR 100 ns;
+    -- expect: sum_out = 0b01000101 = 0x45
+    -- expect: carry_out = 1
 
-		a_in <= "00001111";
-		b_in <= "01010101";
-		carry_in <= '1';
-		WAIT FOR 100 ns;
-		-- expect: sum_out = 0b01100101 = 0x65
-		-- expect: carry_out = 0
+    a_in <= "00001111";
+    b_in <= "01010101";
+    carry_in <= '1';
+    WAIT FOR 100 ns;
+    -- expect: sum_out = 0b01100101 = 0x65
+    -- expect: carry_out = 0
 
-		-- wait forever
-		WAIT;
-	END PROCESS;
+    -- wait forever
+    WAIT;
+  END PROCESS;
 END;
